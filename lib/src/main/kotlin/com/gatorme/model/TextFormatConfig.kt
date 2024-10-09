1ad07142eb780d40a6a732d09211bc475977c037
Gerald Rush <gtrush4@gmail.com>
2024-10-09 14:54:16 -0700

    Fix reverse video bug

--- lib/src/main/kotlin/com/gatorme/model/TextFormatConfig.kt
+++ lib/src/main/kotlin/com/gatorme/model/TextFormatConfig.kt
@@ -7,6 +7,6 @@ import java.io.PrintStream
 
 data class TextFormatConfig(
     val colorOptions: Map<ColorOption, Color> = mapOf(),
-    val textOptions: List<TextOption> = listOf(TextOption.REVERSE_VIDEO),
+    val textOptions: List<TextOption> = listOf(),
     val printStream: PrintStream = System.out,
 )
